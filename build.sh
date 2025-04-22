31244d215766947f3b4f46ab4b3128deb2e724cd
David Acuña <davic1987@gmail.com>
2025-04-22 13:10:06 -0600

    Resolución de conflictos y actualización de archivos 16

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Asegurar que el entorno está limpio
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Instalar portaudio para que PyAudio compile bien
+apt-get update && apt-get install -y portaudio19-dev
+
+# Instalar dependencias del proyecto
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Si estás usando Django (opcional)
+python manage.py migrate
